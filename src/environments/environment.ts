584861b50ec4d9596b8f08dde5d2e07d42a10768
JikkeshRepo <jikkeshwar.p@benhive.com>
2025-06-29 13:15:05 +0530

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://jobsai-server.onrender.com',
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://api.jobsai.in',
     featureFlag: true
 };
